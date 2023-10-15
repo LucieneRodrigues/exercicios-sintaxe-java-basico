12f385dfe5e3f9ec4c715189560b45b72019e6b1
LUCIENE RODRIGUES DA SILVA <cyen@bol.com.br>
2023-10-15 20:00:02 -0300

    Add files via upload

---
+++ src/EqulidrandoSaldo.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class EquilidrandoSaldo {
+	
+	
+	   public static void main(String[] args) {
+		   
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.println("Digite o saldo atual");
+
+	        double saldoAtual = scanner.nextDouble();
+	        System.out.println("Digite o valor de deposito");
+	        double valorDeposito = scanner.nextDouble();
+	        System.out.println("Digite o valor de retirada");
+	        double valorRetirada = scanner.nextDouble();
+	        
+	        saldoAtual = saldoAtual + valorDeposito - valorRetirada;
+	        
+	        System.out.printf("%.1f%n", saldoAtual);
+	        
+	       
+	        
+
+	    
+	}
+}
